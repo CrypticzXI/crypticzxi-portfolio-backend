4e2af9637865b4ce8217c0f25b19b01e3ba2d14f
CrypticzXI <CrypticzXI@gmail.com>
2023-05-16 18:37:40 +0100

    Alter The code of the Portable Text

--- schemas/richText.js
+++ schemas/richText.js
@@ -1,39 +1,44 @@
 export default {
-    name: 'richText',
-    title: 'richText',
-    type: 'array',
-    of: [
-      {
-        type: 'block'
-      },
-      {
-        type: 'iframe',
-        fields: [
-          {
-            type: 'text',
-            options: {
-              isHighlighted: true
-            }
+  name: 'richText',
+  title: 'richText',
+  type: 'array',
+  of: [
+    {
+      type: 'block'
+    },
+    {
+      type: 'image',
+      fields: [
+        {
+          type: 'text',
+          name: 'alt',
+          title: 'Alternative text',
+          description: `Some of your visitors cannot see images, 
+            be they blind, color-blind, low-sighted; 
+            alternative text is of great help for those 
+            people that can rely on it to have a good idea of 
+            what\'s on your page.`,
+          options: {
+            isHighlighted: true
           }
-        ]
-      },
-      {
-        type: 'image',
-        fields: [
-          {
-            type: 'text',
-            name: 'alt',
-            title: 'Alternative text',
-            description: `Some of your visitors cannot see images, 
-              be they blind, color-blind, low-sighted; 
-              alternative text is of great help for those 
-              people that can rely on it to have a good idea of 
-              what\'s on your page.`,
-            options: {
-              isHighlighted: true
-            }
-          },
-        ]
-      }
-    ]
-  }
+        },
+      ]
+    },
+    {
+      type: 'object',
+      name: 'rawHTML',
+      fields: [
+        {
+          type: 'text',
+          name: 'html',
+          title: 'HTML',
+          description: 'Enter raw HTML code',
+          validation: Rule => Rule.required(),
+          options: {
+            isHighlighted: true
+          }
+        }
+      ]
+    }
+  ]
+}
